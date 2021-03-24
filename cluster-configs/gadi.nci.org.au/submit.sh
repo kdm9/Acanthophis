450475451b4db02e73b2f15874a0d0023db34e96
Kevin Murray <foss@kdmurray.id.au>
2021-03-24 21:23:02 +1100

    Add gadi profile

--- cluster-configs/gadi.nci.org.au/submit.sh
+++ cluster-configs/gadi.nci.org.au/submit.sh
@@ -10,41 +10,14 @@
 #PBS -P xe2
 
 set -ueo pipefail
-logdir=data/log/cluster/
-mkdir -p $logdir
-export TMPDIR=${PBS_JOBFS:-$TMPDIR}
 TARGET=${TARGET:-all}
-SNAKEFILE=${SNAKEFILE:-Snakefile}
-
-QSUB="qsub -q {cluster.queue} -l ncpus={threads} -l jobfs={cluster.jobfs}"
-QSUB="$QSUB -l walltime={cluster.time} -l mem={cluster.mem} -N {cluster.name} -l storage=scratch/{cluster.project}+gdata/{cluster.project}"
-QSUB="$QSUB -l wd -j oe -o $logdir -P {cluster.project}"
-
-if [ "${RMTEMP:-yes}" == yes ]
-then
-	temp=''
-else
-	temp='--notemp'
-fi
 
 snakemake \
     --use-conda \
     --conda-frontend mamba \
     --conda-create-envs-only \
-    --snakefile "$SNAKEFILE"                                       \
 
-snakemake                                                          \
-    -j 1000                                                        \
-    --use-conda                                                    \
-    --cluster-config gadi/cluster.yaml                           \
-    --local-cores ${PBS_NCPUS:-1}                                  \
-    --js gadi/jobscript.sh                                       \
-    --nolock                                                       \
-    --rerun-incomplete                                             \
-    --keep-going                                                   \
-    $temp                                                          \
-    --snakefile "$SNAKEFILE"                                       \
-    --cluster "$QSUB"                                              \
-    "$TARGET"                                                      \
-    |& tee data/log/submitter_${PBS_JOBID:-headnode}_snakemake.log \
-
+snakemake               \
+    -j 1000             \
+    --profile ./gadi/   \
+    "$TARGET"
