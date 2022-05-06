62ec141b9bcadb0753f69e8ce6d0fd6c9caae6fc
Kevin Murray <foss@kdmurray.id.au>
2022-05-06 16:54:56 +0200

    Aborted attempt to fix CI
    Squashed commit of the following:
    
    commit 46573fd4b14452577d64dc34cb96b81ab6cc5d6c
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:50:53 2022 +0200
    
        df
    
    commit 82081c7d6f3bf2bf418eb7f7ef6f8c692a010200
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:45:22 2022 +0200
    
        fixup! fixup! fixup! fixup! fixup! tryfix: ci
    
    commit cc6a462d0a02087dd2257da59b33d55042a3c9c5
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:42:59 2022 +0200
    
        fixup! fixup! fixup! fixup! tryfix: ci
    
    commit 555aa1c7ca48aea5bf012b6785964483aa7eda32
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:40:00 2022 +0200
    
        fixup! fixup! fixup! tryfix: ci
    
    commit 121032259d2a794df00b14df2493673ee1d6c6bb
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:31:50 2022 +0200
    
        fixup! fixup! tryfix: ci
    
    commit 33aedf25eba3f01c2a1c0a2e1e09d958d4cac25c
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:29:14 2022 +0200
    
        fixup! tryfix: ci
    
    commit 7f0de19dae87215890569d6b9785fcabb8a9e44f
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:27:53 2022 +0200
    
        tryfix: ci
    
    commit f5e41d9f08d9aab0da5de9284140c0153681e877
    Author: Kevin Murray <foss@kdmurray.id.au>
    Date:   Fri May 6 16:25:31 2022 +0200
    
        tryfix: ci

--- example/setup.sh
+++ example/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #set -xeuo pipefail
-mamba env update -f environment.yml
-mamba activate acanthophis-demo
+mamba env update -f environment_demo.yml
+conda activate acanthophis-demo
 pip install -e ../
 acanthophis-init --yes
 # download kraken DB
 mkdir -p data/dbs/kraken/viral
 pushd  data/dbs/kraken/viral
-test -f  k2_viral_20201202.tar.gz || wget https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20201202.tar.gz
+test -f  k2_viral_20201202.tar.gz || wget -q https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20201202.tar.gz
 tar xvf k2_viral_20201202.tar.gz
 popd
 snakemake --snakefile Snakefile.generate-rawdata -j 4 --use-conda --conda-frontend mamba
