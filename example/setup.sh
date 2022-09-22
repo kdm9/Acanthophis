4b4a225b7ccdf463745ac5fb928e59499d046e49
Dr. K.D. Murray <foss@kdmurray.id.au>
2022-09-22 13:10:51 +0200

    example: move krakendb build to snakefile

--- example/setup.sh
+++ example/setup.sh
@@ -4,11 +4,4 @@ mamba env update -f environment-setup.yml
 conda activate acanthophis-example
 pip install -e ../
 acanthophis-init --yes
-# download kraken DB
-mkdir -p rawdata/kraken/viral
-pushd rawdata/kraken/viral
-test -f  k2_viral_20201202.tar.gz || wget -q https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20201202.tar.gz
-tar xvf k2_viral_20201202.tar.gz
-rm -f k2_viral_20201202.tar.gz
-popd
 snakemake --snakefile Snakefile.generate-rawdata -j 4 --use-conda --conda-frontend mamba
