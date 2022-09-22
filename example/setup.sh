1d5b6b61d0f063ae919a8b9d789c6a7dddfb1497
Dr. K.D. Murray <foss@kdmurray.id.au>
2022-09-22 11:26:25 +0200

    Use viral kraken db

--- example/setup.sh
+++ example/setup.sh
@@ -5,9 +5,10 @@ conda activate acanthophis-demo
 pip install -e ../
 acanthophis-init --yes
 # download kraken DB
-mkdir -p data/dbs/kraken/viral
-pushd  data/dbs/kraken/viral
+mkdir -p rawdata/kraken/viral
+pushd rawdata/kraken/viral
 test -f  k2_viral_20201202.tar.gz || wget -q https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20201202.tar.gz
 tar xvf k2_viral_20201202.tar.gz
+rm -f k2_viral_20201202.tar.gz
 popd
 snakemake --snakefile Snakefile.generate-rawdata -j 4 --use-conda --conda-frontend mamba
