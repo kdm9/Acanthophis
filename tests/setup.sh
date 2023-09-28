0405914aeb5ddda9675d0f2d1cafbd60904652ad
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-09-28 07:46:38 +0200

    setup & tests: use conda

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mamba env update -f environment-setup.yml
-mamba activate acanthophis-tests
+conda activate acanthophis-tests
 rm -rf output/ tmp/
 pip install -e ../
 set -xeuo pipefail
--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mamba activate acanthophis-tests
+conda activate acanthophis-tests
 set -xeuo pipefail
 python3 -m pip install ..
 acanthophis-init --yes
 mamba env update -f environment.yml
-mamba activate acanthophis
+conda activate acanthophis
 rm -fr output tmp
 snakemake -j 8 --use-conda --ri "${@}"
