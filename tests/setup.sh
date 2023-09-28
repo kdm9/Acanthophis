f0b4b3a90abfe252b1c9b779e1f4d4e346a4e1f9
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-09-28 07:39:55 +0200

    Fix merge issues with last import

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mamba env update -f environment-setup.yml
-conda activate acanthophis-tests
+mamba activate acanthophis-tests
 rm -rf output/ tmp/
 pip install -e ../
 set -xeuo pipefail
--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-conda activate acanthophis-tests
+mamba activate acanthophis-tests
 set -xeuo pipefail
+python3 -m pip install ..
 acanthophis-init --yes
 mamba env update -f environment.yml
-conda activate acanthophis
+mamba activate acanthophis
 rm -fr output tmp
 snakemake -j 8 --use-conda --ri "${@}"
