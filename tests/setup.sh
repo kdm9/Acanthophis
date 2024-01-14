825085763eb5f07ac399520c29071cd3f979f870
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-01-14 11:36:04 +0100

    cleaning and fixing of test dataset generation

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mamba env update -f environment-setup.yml
-conda activate acanthophis-tests
+mamba activate acanthophis-tests
 rm -rf output/ tmp/
+mkdir -p output/ tmp/ rawdata/
+touch output/nobackup tmp/nobackup rawdata/nobackup
 pip install -e ../
 set -xeuo pipefail
 snakemake --snakefile Snakefile.generate-rawdata -j 4 --use-conda --conda-frontend mamba
