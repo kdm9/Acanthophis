540c5546f420870e52b6876bff190e8012e05030
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-01-16 17:52:18 +0100

    better test script structure

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 mamba env update -f environment-setup.yml
 conda activate acanthophis-tests
+set -xeuo pipefail
 rm -rf output/ tmp/
 mkdir -p output/ tmp/ rawdata/
 touch output/nobackup tmp/nobackup rawdata/nobackup
-python3 -m pip install -e ../
-set -xeuo pipefail
-snakemake --version
-snakemake --snakefile Snakefile.generate-rawdata -j 4 --software-deployment-method conda
+snakemake --snakefile Snakefile.generate-rawdata -j $(nproc 2>/dev/null || echo 2) --software-deployment-method conda
 tree rawdata
--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 set -euo pipefail
+
+# Reinstall acanthophis from git
 conda activate acanthophis-tests
 python3 -m pip uninstall --yes acanthophis
 python3 -m pip install -e ..
+
+# re-init after reinstall
 acanthophis-init --yes
+
+# recreate the conda env
 mamba env update -f environment.yml
 conda activate acanthophis
+
+# Clear outputs
 rm -fr output tmp
-set -x
+touch output/nobackup tmp/nobackup
+
+# If quick, overwrite config with cut-back version
+if [[ "$1" == "--quick" ]]
+then
+    cp .config_quick.yml config.yml
+    shift
+fi
+
+# Run pipeline
 snakemake -j $(nproc 2>/dev/null || echo 2)  --software-deployment-method conda --ri "${@}"
