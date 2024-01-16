83a36254931f8bc1aa8efcc1c19397ac72d246df
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-01-16 14:25:59 +0100

    GHA: disable deep variant

--- tests/test.sh
+++ tests/test.sh
@@ -8,4 +8,4 @@ mamba env update -f environment.yml
 conda activate acanthophis
 rm -fr output tmp
 set -x
-snakemake -j $(nproc 2>/dev/null || echo 2)  --software-deployment-method conda apptainer --ri "${@}"
+snakemake -j $(nproc 2>/dev/null || echo 2)  --software-deployment-method conda --ri "${@}"
