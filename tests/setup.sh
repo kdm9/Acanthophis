3ef9746c1409f367ba4715b8f7a87676e84b8004
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-01-28 20:52:02 +0100

    nuke all outputs before tests

--- tests/setup.sh
+++ tests/setup.sh
@@ -2,7 +2,7 @@
 mamba env update -f environment-setup.yml
 conda activate acanthophis-tests
 set -xeuo pipefail
-rm -rf output/ tmp/
+git clean -fdx || echo "WARNING: git clean failed, beware stale files"
 mkdir -p output/ tmp/ rawdata/
 touch output/nobackup tmp/nobackup rawdata/nobackup
 snakemake --snakefile Snakefile.generate-rawdata -j $(nproc 2>/dev/null || echo 2) --software-deployment-method conda
--- tests/test.sh
+++ tests/test.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-set -euo pipefail
 
 # Reinstall acanthophis from git
 conda activate acanthophis-tests
+set -eo pipefail
 python3 -m pip uninstall --yes acanthophis
 python3 -m pip install -e ..
 
 # re-init after reinstall
+rm -rf output/ tmp/ workflow config.yml Snakefile environment.yml
+mkdir -p output/ tmp/
+touch output/nobackup tmp/nobackup
 acanthophis-init --yes
 
 # recreate the conda env
 mamba env update -f environment.yml
 conda activate acanthophis
 
-# Clear outputs
-rm -fr output tmp
-mkdir -p output/ tmp/
-touch output/nobackup tmp/nobackup
 
 # If quick, overwrite config with cut-back version
 SDM=(conda)
